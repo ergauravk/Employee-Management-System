11aa2d5f8738dc58e41c5d4a8a513df10212c12c
ergauravk <ergauravkumar2005@gmail.com>
2026-05-06 13:51:17 +0530

    de

--- manage.py
+++ manage.py
@@ -6,6 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.join(base_dir, "employee")
+    if project_dir not in sys.path:
+        sys.path.insert(0, project_dir)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "employee.settings")
     try:
         from django.core.management import execute_from_command_line
